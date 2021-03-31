2f3166d5cc0989cc90231445c1e51257914be5ff
emanuelvictor <emanuelvictor@msn.com>
2021-03-31 17:17:01 -0300

    Trying fix bug in starting of browser

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const puppeteer = require('puppeteer');
 const app = express();
 
-app.get('/', (req, res) => res.send('Home Page Route'));
+app.get('/', (req, res) => res.send('FOI'));
 
 app.get('/about', async (req, res) => {
 
