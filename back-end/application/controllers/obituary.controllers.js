8f37ef25ff73341676251b7cc06ea05beff2b4d8
emanuelvictor <emanuelvictor@msn.com>
2021-03-30 21:18:31 -0300

    Trying fix bug in starting of browser

--- back-end/application/controllers/obituary.controllers.js
+++ back-end/application/controllers/obituary.controllers.js
@@ -10,38 +10,38 @@ exports.get = async (req, res) => {
 
     let data = [];
     let elements = document.getElementsByClassName('LabelTitulo');
-        console.log('elemtns length', elements.length)
-    for (var i = 0; i < elements.length; i++) {
-        death = {
-            person : {}
-        };
-
-        death.person.name = document.getElementById('wucSircObituario_DataList1_dtlFalecimentos_lblNome0_' + i).textContent
-        death.person.age = document.getElementById('wucSircObituario_DataList1_dtlFalecimentos_lblIdade0_' + i).textContent.toLowerCase().replace(' anos', '') //TODO ISOLATE
-
-        const rawDate = document.getElementById('wucSircObituario_DataList1_dtlFalecimentos_Label14_' + i).textContent.toLowerCase()
-        death.date = rawDate.substring(0, rawDate.indexOf('à') -1)
-
-        const cause = document.getElementById('wucSircObituario_DataList1_dtlFalecimentos_Label13_' + i).textContent
-
-        if(cause.indexOf('COVID - 19') > -1){
-
-          death.covid = true
-
-          if(cause.indexOf('NÃO DETECTADO') > -1)
-            death.covid = false
-
-        }
-        else
-          death.covid = false
-
-        if(cause.indexOf('AGUARDANDO') > -1)
-          death.covid = undefined
-
-        data.push(death)
-    }
-
-    return data;
+    return elements;
+//    for (var i = 0; i < elements.length; i++) {
+//        death = {
+//            person : {}
+//        };
+//
+//        death.person.name = document.getElementById('wucSircObituario_DataList1_dtlFalecimentos_lblNome0_' + i).textContent
+//        death.person.age = document.getElementById('wucSircObituario_DataList1_dtlFalecimentos_lblIdade0_' + i).textContent.toLowerCase().replace(' anos', '') //TODO ISOLATE
+//
+//        const rawDate = document.getElementById('wucSircObituario_DataList1_dtlFalecimentos_Label14_' + i).textContent.toLowerCase()
+//        death.date = rawDate.substring(0, rawDate.indexOf('à') -1)
+//
+//        const cause = document.getElementById('wucSircObituario_DataList1_dtlFalecimentos_Label13_' + i).textContent
+//
+//        if(cause.indexOf('COVID - 19') > -1){
+//
+//          death.covid = true
+//
+//          if(cause.indexOf('NÃO DETECTADO') > -1)
+//            death.covid = false
+//
+//        }
+//        else
+//          death.covid = false
+//
+//        if(cause.indexOf('AGUARDANDO') > -1)
+//          death.covid = undefined
+//
+//        data.push(death)
+//    }
+//
+//    return data;
   });
 
   await browser.close();
