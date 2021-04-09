7aadd509120d3de1d0564728321c58833399bfb7
emanuelvictor <emanuelvictor@msn.com>
2021-04-09 01:01:57 -0300

    Trying fix bug in starting of browser

--- back-end/application/controllers/obituary.controllers.js
+++ back-end/application/controllers/obituary.controllers.js
@@ -13,53 +13,53 @@ try {
    const page = await browser.newPage();
    await page.goto('http://www3.pmfi.pr.gov.br/PSIPortal/SircofWeb/Formularios/wfrmSircObituario_Site.aspx')
 
-   await page.screenshot({ path: __dirname + '\\example.png', fullPage: true });
+//   await page.screenshot({ path: __dirname + '\\example.png', fullPage: true });
 
- //  let deaths = await page.evaluate(() => {
- //
- //    let data = [];
- //
- ////    let elements = JSON.parse(document.getElementsByTagName('pre')[0].textContent); //document.getElementsByClassName('LabelTitulo');
- ////return elements;
- //    let elements = document.getElementsByClassName('LabelTitulo');
- //
- //    for (var i = 0; i < elements.length; i++) {
- //        death = {
- //            person : {}
- //        };
- //
- //        death.person.name = document.getElementById('wucSircObituario_DataList1_dtlFalecimentos_lblNome0_' + i).textContent
- //        death.person.age = document.getElementById('wucSircObituario_DataList1_dtlFalecimentos_lblIdade0_' + i).textContent.toLowerCase().replace(' anos', '') //TODO ISOLATE
- //
- //        const rawDate = document.getElementById('wucSircObituario_DataList1_dtlFalecimentos_Label14_' + i).textContent.toLowerCase()
- //        death.date = rawDate.substring(0, rawDate.indexOf('à') -1)
- //
- //        const cause = document.getElementById('wucSircObituario_DataList1_dtlFalecimentos_Label13_' + i).textContent
- //
- //        if(cause.indexOf('COVID - 19') > -1){
- //
- //          death.covid = true
- //
- //          if(cause.indexOf('NÃO DETECTADO') > -1)
- //            death.covid = false
- //
- //        }
- //        else
- //          death.covid = false
- //
- //        if(cause.indexOf('AGUARDANDO') > -1)
- //          death.covid = undefined
- //
- //        data.push(death)
- //    }
- //
- //    return data;
- //  });
-         res.sendFile(__dirname + '\\example.png');
+   let deaths = await page.evaluate(() => {
+
+     let data = [];
+
+ //    let elements = JSON.parse(document.getElementsByTagName('pre')[0].textContent); //document.getElementsByClassName('LabelTitulo');
+ //return elements;
+     let elements = document.getElementsByClassName('LabelTitulo');
+
+     for (var i = 0; i < elements.length; i++) {
+         death = {
+             person : {}
+         };
+
+         death.person.name = document.getElementById('wucSircObituario_DataList1_dtlFalecimentos_lblNome0_' + i).textContent
+         death.person.age = document.getElementById('wucSircObituario_DataList1_dtlFalecimentos_lblIdade0_' + i).textContent.toLowerCase().replace(' anos', '') //TODO ISOLATE
+
+         const rawDate = document.getElementById('wucSircObituario_DataList1_dtlFalecimentos_Label14_' + i).textContent.toLowerCase()
+         death.date = rawDate.substring(0, rawDate.indexOf('à') -1)
+
+         const cause = document.getElementById('wucSircObituario_DataList1_dtlFalecimentos_Label13_' + i).textContent
+
+         if(cause.indexOf('COVID - 19') > -1){
+
+           death.covid = true
+
+           if(cause.indexOf('NÃO DETECTADO') > -1)
+             death.covid = false
+
+         }
+         else
+           death.covid = false
+
+         if(cause.indexOf('AGUARDANDO') > -1)
+           death.covid = undefined
+
+         data.push(death)
+     }
+
+     return data;
+   });
+res.status(200).send(deaths)
 }
 catch (e) {
- // statements to handle any exceptions
+res.status(500).send('erro')
  }
 
-//  res.status(200).send(deaths)
+
 };
